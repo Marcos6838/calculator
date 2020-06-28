9d6591a6ab349fa151dd49988f970d84e10bea9e
Marcos Hernandez <marcos510hernandez@gmail.com>
2020-06-27 20:43:04 -0700

    added functions for add, subtract, multiply, divide

---
+++ script.js
@@ -0,0 +1,24 @@
+function add(num1, num2){
+    let answer = num1 + num2;
+    return answer;
+}
+
+function subtract(num1, num2) {
+    let answer = num1 - num2;
+    return answer;
+}
+
+function multiply(num1, num2) {
+    let answer = num1 * num2;
+    return answer;
+}
+
+function divide(num1, num2) {
+    let answer = num1 / num2;
+    return answer;
+}
+
+console.log(add(5, 5));
+console.log(subtract(10, 13))
+console.log(multiply(5, 5))
+console.log(divide(100,10))
