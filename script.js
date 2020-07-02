eff8883f4864c0a47e112e385e8190d3bd890325
Marcos Hernandez <marcos510hernandez@gmail.com>
2020-07-01 19:33:11 -0700

    added event listeners for all numbers

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+let one = document.querySelector('#one');
+let two = document.querySelector('#two');
+let three = document.querySelector('#three');
+let four = document.querySelector('#four');
+let five = document.querySelector('#five');
+let six = document.querySelector('#six');
+let seven = document.querySelector('#seven');
+let eight = document.querySelector('#eight')
+let nine = document.querySelector('#nine');
+let zero = document.querySelector('#zero');
+
 function add(num1, num2){
     let answer = num1 + num2;
     return answer;
@@ -39,3 +50,51 @@ function opChoice(num1, num2, operator){
 
 opChoice(5, 10, "*");
 
+one.addEventListener('click',function(e){
+    console.log(e);
+})
+
+
+two.addEventListener('click',function(e){
+    console.log(e);
+})
+
+
+three.addEventListener('click',function(e){
+    console.log(e);
+})
+
+
+four.addEventListener('click',function(e){
+    console.log(e);
+})
+
+
+five.addEventListener('click',function(e){
+    console.log(e);
+})
+
+
+six.addEventListener('click',function(e){
+    console.log(e);
+})
+
+
+seven.addEventListener('click',function(e){
+    console.log(e);
+})
+
+
+eight.addEventListener('click',function(e){
+    console.log(e);
+})
+
+
+nine.addEventListener('click',function(e){
+    console.log(e);
+})
+
+
+zero.addEventListener('click',function(e){
+    console.log(e);
+})
